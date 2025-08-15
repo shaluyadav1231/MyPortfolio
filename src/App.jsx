19cc93284e7177a596d4a7662f6a379a9a45a320
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-08-15 09:51:46 +0530

    half navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import style from "./App.module.css"  
+import { Navbar } from "./components/Navbar/navbar"
+
 
 function App() {
   
 
   return (
     <>
-       <div>
-      hiiiii!!!!!!!!!
+       <div className={style.App}> 
+         <Navbar />
        </div>
         
     </>
